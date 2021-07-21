6bb9e419088598e12b7d4ed4e897cb3fc1ce8457
Mackenzie Kroon <kroonmackenzie@gmail.com>
2021-07-21 16:08:20 -0700

    fixed css issue

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,9 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
+// import "bootswatch/dist/minty/bootstrap.min.css";
+import Header from "./layout/Header";
 
 class App extends React.Component {
   render() {
-    return <h1>Frontend!!!</h1>;
+    return (
+      <div>
+        <Header />
+        <h1>Content</h1>
+      </div>
+    );
   }
 }
 
