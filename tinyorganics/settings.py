8ef19fad7a4ca5a39b084ec7eaa18889eccd6302
Mackenzie Kroon <kroonmackenzie@gmail.com>
2021-07-21 15:06:44 -0700

    frontend is connected

--- tinyorganics/settings.py
+++ tinyorganics/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'recipe',
-    'rest_framework'
+    'rest_framework',
+    'frontend'
 ]
 
 MIDDLEWARE = [
